8085ff61201196e31c99ea8a9c1e1b274c3b993c
leonardo.franklin <leo22282@gmail.com>
2024-09-06 00:10:49 -0300

    Correcao CharField

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Category, Recipe
 
 # Register your models here.
 #Aqui você informa as tabelas que aparecem no http://127.0.0.1:8000/admin
 
 class CategoryAdmin(admin.ModelAdmin):
     ...
-    
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+
 admin.site.register(Category, CategoryAdmin)
