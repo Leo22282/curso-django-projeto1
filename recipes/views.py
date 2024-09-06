c936823f37277c533ab929e78ea7b9e6d50494af
leonardo.franklin <leo22282@gmail.com>
2024-09-05 22:49:55 -0300

    Separar Arquivos e colocar NavBar

--- recipes/views.py
+++ recipes/views.py
@@ -3,12 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'Leonardo Franklin',
     })
-
-def contato(request):
-    return HttpResponse("<b>CONTATO</b>")
-
-def sobre(request):
-    return HttpResponse("<b>SOBRE</b>")
